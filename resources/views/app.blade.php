d9710f60b82dfc9eac29cd1bcde25b2e538e249e
Agony <agony@localhost.localdomain>
2020-12-05 16:40:49 -0300

    produccion

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
-        <title>Crud Inertia</title>
+        <title>Young Problems - comunidad de gente con problemas</title>
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
         <!-- Scripts -->
