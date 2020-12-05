c2814bbbb8c820e88f7737cda226f41b7a7dd170
Agony <agony@localhost.localdomain>
2020-12-05 17:10:02 -0300

    produccion

--- config/mail.php
+++ config/mail.php
@@ -36,11 +36,11 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
-            'port' => env('MAIL_PORT', 587),
-            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-            'username' => env('MAIL_USERNAME'),
-            'password' => env('MAIL_PASSWORD'),
+            'host' => env('MAIL_HOST', 'smtp.googlemail.com'),
+            'port' => env('MAIL_PORT', 465),
+            'encryption' => env('MAIL_ENCRYPTION', 'ssl'),
+            'username' => env('nahuel.dev.bot@gmail.com'),
+            'password' => env('toiccjvzflfeisnz'),
             'timeout' => null,
             'auth_mode' => null,
         ],
