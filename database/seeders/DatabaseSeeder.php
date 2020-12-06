3cd868897864bf77ea74a578cee91e8e5b96bd8f
Agony <agony@localhost.localdomain>
2020-12-06 07:52:41 -0300

    truncate

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,9 @@ class DatabaseSeeder extends Seeder
 
     public function run()
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        DB::table('categories')->truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
         Category::factory()->create([
             "category" => "General",
         ]);
