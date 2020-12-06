47d1e84754c28c47586a08218dcf411252b7d780
Agony <agony@localhost.localdomain>
2020-12-06 07:43:54 -0300

    truncate

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,6 @@ class DatabaseSeeder extends Seeder
 
     public function run()
     {
-        DB::table('categories')->truncate();
-
         Category::factory()->create([
             "category" => "General",
         ]);
